eb3dda32f82df211c72b7760bb11b9ce25b24587
sanagimt <180413622+sanagimt@users.noreply.github.com>
2025-03-19 07:43:58 +0000

    controller関係

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
+  get 'users/edit'
   devise_for :users
   root to: 'homes#top'
+  resources :books, only: [:new, :create, :index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
