fb6aaa4d4f1a8f9755ebcc4f885d100d79752a6b
sanagimt <180413622+sanagimt@users.noreply.github.com>
2025-03-22 06:56:35 +0000

    完成2

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,6 +17,6 @@ class User < ApplicationRecord
     profile_image.variant(resize_to_limit: [width, height]).processed
   end
 
-  validates :name, presence: true, length: { in: 2..20 }, uniqueness: true
+  validates :name, length: { in: 2..20 }, uniqueness: true
   validates :introduction, length: { maximum: 50 }
 end
